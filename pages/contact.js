f85e4161db76b6f0ac6096413e77c0f7f78279a1
Adam Wright <adam.wright90@yahoo.co.uk>
2021-09-26 20:03:46 +0100

    fixes

--- pages/contact.js
+++ pages/contact.js
@@ -11,15 +11,17 @@ function Contact() {
             <NextSeo title="Contact page" />
             <motion.div initial="initial" animate="enter" exit="exit" className="l-grid-maincontent">
                 <motion.div variants={fade}>
-                    <h1 className="heading">Contact</h1>
-                    <br />
-                    <p className="para">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lectus eu faucibus accumsan. Nulla erat metus, sagittis et dolor id, ultrices venenatis metus. Vivamus
-                        ultrices massa ut lectus cursus sollicitudin. Suspendisse at purus vulputate, venenatis magna a, gravida quam. Vivamus non congue nibh, nec dignissim lectus. Morbi id odio
-                        auctor tellus blandit dignissim. Sed non convallis orci. Nulla facilisis tellus at tellus vehicula gravida. Nam ex diam, eleifend sit amet consectetur non, finibus posuere
-                        velit. Nunc ut augue varius, dapibus risus at, malesuada ligula.
-                    </p>
-                    <ContactForm />
+                    <div data-scroll-section>
+                        <h1 className="heading">Contact</h1>
+                        <br />
+                        <p className="para">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lectus eu faucibus accumsan. Nulla erat metus, sagittis et dolor id, ultrices venenatis metus.
+                            Vivamus ultrices massa ut lectus cursus sollicitudin. Suspendisse at purus vulputate, venenatis magna a, gravida quam. Vivamus non congue nibh, nec dignissim lectus. Morbi
+                            id odio auctor tellus blandit dignissim. Sed non convallis orci. Nulla facilisis tellus at tellus vehicula gravida. Nam ex diam, eleifend sit amet consectetur non, finibus
+                            posuere velit. Nunc ut augue varius, dapibus risus at, malesuada ligula.
+                        </p>
+                        <ContactForm />
+                    </div>
                 </motion.div>
             </motion.div>
         </Layout>
